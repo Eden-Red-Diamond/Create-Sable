82825e37b729c30ae10b214f225f46f733e157ec
Roise <roisecortez2@gmail.com>
2026-06-27 04:09:58 +0800

    Update main.js of startup

--- kubejs/startup_scripts/main.js
+++ kubejs/startup_scripts/main.js
@@ -1,34 +1,16 @@
+const layoutData = JsonIO.read('kubejs/config/creative_layout.json') || { gearworks_items: ['create:hand_crank']};
+const orderedItems = layoutData.gearworks_items;
+
 StartupEvents.registry('creative_mode_tab', event =>{
     // Creative Tab
     event.create('create_gearworks')
          .icon(() => 'create:large_cogwheel')
          .displayName(Text.darkAqua('Create Gearworks'))
-         .content(showRestrictedItems => [
-            'create:hand_crank'
-         ])
+         .content(showRestrictedItems => [ orderedItems[0] ])
 })
 
 StartupEvents.modifyCreativeTab('kubejs:create_gearworks', event => {
-    let orderedItems = [
-        'create:speedometer',
-        'create:stressometer',
-        'create:belt_connector',
-        'create:shaft',
-        'create:cogwheel',
-        'create:large_cogwheel',
-        'create:gearbox',
-        'create:vertical_gearbox',
-        'create:clutch',
-        'create:gearshift',
-        'create:encased_chain_drive',
-        'create:adjustable_chain_gearshift',
-        'create:water_wheel',
-        'create:large_water_wheel',
-        'create:windmill_bearing',
-        'simulated:red_portable_engine',
-        'create:steam_engine'
-    ]
-    orderedItems.forEach(item => {
-        event.add(item)
-    })
+    for (let i = 1; i < orderedItems.lenght; i++) {
+        event.add(orderedItems[i])
+        }
 })
